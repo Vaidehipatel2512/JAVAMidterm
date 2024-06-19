777c1ed6956c51e579687551bd960cb376999bcd
Vaidehi Patel <147642618+Vaidehipatel2512@users.noreply.github.com>
2024-06-19 15:33:03 -0400

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.fx200575777 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.fx200575777 to javafx.fxml;
-    exports com.example.fx200575777;
-}
